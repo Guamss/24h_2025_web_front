003ec9015b844094d80437ef587906b315812cc6
guams <guams@duck.com>
2025-05-20 21:57:21 +0200

    ajout du dockerfile

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,6 @@
 import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
 import {provideRouter} from '@angular/router';
-
 import {routes} from './app.routes';
-import {provideClientHydration} from '@angular/platform-browser';
 import {provideHttpClient, withFetch} from '@angular/common/http';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {providePrimeNG} from 'primeng/config';
